5d94b7075cf72a0131f8127c06a1c16a9d4b4e3f
1shaked <shaked1hen@gmail.com>
2023-01-31 13:31:46 +0200

    first commit

---
+++ df_columns.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+df = pd.read_csv('Top 50 Youtube Channels.csv')
+
+
+
+def func(col: pd.Series):
+    '''
+    here you get a Series of all the elements as a 
+    '''
+    print(type(col))
+    print(col.name)
+    print(col)
+    col = col.apply(lambda v: str(v).split(' ')[0])
+    return col
+
+cols = ['Network', 'Channel name']
+df.loc[:, cols] = df.loc[:, cols].apply(func)
+
+df['Network']
