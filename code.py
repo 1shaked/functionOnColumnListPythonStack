4d0b62a5e1d0ad72043fb608b06b2da5e33022a7
1shaked <shaked1hen@gmail.com>
2023-01-31 13:33:07 +0200

    change files name

--- code.py
+++ code.py
@@ -8,13 +8,12 @@ def func(col: pd.Series):
     '''
     here you get a Series of all the elements as a 
     '''
-    print(type(col))
-    print(col.name)
-    print(col)
+    # print(type(col))
+    # print(col.name)
+    # print(col)
     col = col.apply(lambda v: str(v).split(' ')[0])
     return col
 
 cols = ['Network', 'Channel name']
 df.loc[:, cols] = df.loc[:, cols].apply(func)
 
-df['Network']
